fd8f6554acaf8bb757083978f6e2af37ddf5a27d
murat aslan <murataslanx33@gmail.com>
2020-12-21 22:29:55 +0300

    add left-sidebar

---
+++ components/left-sidebar/left-sidebar.js
@@ -0,0 +1,7 @@
+import Link from "next/link";
+import style from "./left-sidebar.module.css";
+import { Search } from "@material-ui/icons";
+
+export default function LeftSidebar() {
+  return <div id={style.sidebar}></div>;
+}
--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,13 @@ import Head from "next/head";
 import Header from "../components/header";
 import styles from "../styles/Index.module.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import LeftSidebar from "../components/left-sidebar/left-sidebar";
 
 export default function Index() {
   return (
     <div className={styles.container}>
       <Header />
+      <LeftSidebar />
     </div>
   );
 }
